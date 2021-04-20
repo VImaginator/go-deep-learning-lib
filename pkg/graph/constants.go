1c6ea412e30f174da214e899e3726233f40023da
VImaginator <vimaginator@proton.me>
2021-04-20 10:34:12 +0000

    :arrow_up: 1.1.9 :tada:

--- pkg/graph/constants.go
+++ pkg/graph/constants.go
@@ -3,4 +3,5 @@ package graph
 const (
 	DefaultBatchSize    uint64               = 1
 	DefaultLearningRate ConstantLearningRate = 0.01
-	DefaultSeed         int64                = 98
+	DefaultSeed         int64                = 9868
+	epsilon            
