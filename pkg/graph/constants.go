7d63c67bd8ded5e4ae8fcc2f54d84b36ad0bd469
VImaginator <vimaginator@proton.me>
2021-04-21 05:57:22 +0000

    Documentation refactor.

--- pkg/graph/constants.go
+++ pkg/graph/constants.go
@@ -4,4 +4,4 @@ const (
 	DefaultBatchSize    uint64               = 1
 	DefaultLearningRate ConstantLearningRate = 0.01
 	DefaultSeed         int64                = 9868
-	epsilon            
+	epsilon             float64           
