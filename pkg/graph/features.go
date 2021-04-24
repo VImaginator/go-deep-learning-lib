178e9786afd79295118e11424064f05326fcb16a
VImaginator <vimaginator@proton.me>
2021-04-24 13:43:06 +0000

    Remove unused attribute

--- pkg/graph/features.go
+++ pkg/graph/features.go
@@ -9,3 +9,29 @@ type Features struct {
 	ClassWeights        []float64
 	DisableClassWeights bool
 	DisableShuffle      bool
+	X                   [][]float64
+	Y                   [][]float64
+}
+
+func (f *Features) Balance() {
+	if len(f.Y) == 0 || len(f.Y[0]) == 0 {
+		return
+	}
+	n := len(f.Y)
+	m := len(f.Y[0])
+	f.ClassWeights = make([]float64, m)
+	for i := range f.Y {
+		f.ClassWeights[argmax(f.Y[i])]++
+	}
+	for i := range f.ClassWeights {
+		f.ClassWeights[i] = float64(n) / (float64(m) * f.ClassWeights[i])
+	}
+}
+
+func (f *Features) Prepare() {
+	if !f.DisableShuffle {
+		f.Shuffle()
+	}
+	if !f.DisableClassWeights {
+		f.Balance()
+	}
