c693a26580f77f1275a10ec6ad6f58527355bd4f
VImaginator <vimaginator@proton.me>
2021-04-24 22:45:19 +0000

    Fix migration.

--- pkg/graph/features_test.go
+++ pkg/graph/features_test.go
@@ -9,3 +9,23 @@ import (
 
 func TestFeaturesClassWeights(t *testing.T) {
 	for k, v := range []struct {
+		samples  int
+		classes  int
+		features int
+		minority int
+		weights  []float64
+	}{
+		{43400, 5, 21, 783, []float64{0.8159428463996992, 0.8102305610006534, 0.8234512854567878, 0.8093240093240093, 11.085568326947637}},
+	} {
+		t.Run(strconv.Itoa(k), func(t *testing.T) {
+			features := Features{
+				X: make([][]float64, v.samples),
+				Y: make([][]float64, v.samples),
+			}
+			rand.Seed(80)
+			n := v.classes - 1
+			for i := 0; i < v.samples; i++ {
+				features.X[i] = make([]float64, v.features)
+				features.Y[i] = make([]float64, v.classes)
+				if i < v.minority {
+					features.Y[i][n] = 1
