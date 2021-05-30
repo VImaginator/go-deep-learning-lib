7da03a5f31b483f8400a3ccf175f27acd4214990
VImaginator <vimaginator@proton.me>
2021-05-30 13:26:09 +0000

    Update pytest from 3.4.1 to 3.4.2

--- pkg/graph/fitter.go
+++ pkg/graph/fitter.go
@@ -15,3 +15,4 @@ func (f Fitter) Fit(g Graph, w ...MetricsWriter) {
 	for i := 1; i <= int(f.Epochs); i++ {
 		for j := range f.Training.X {
 			a := g.Estimate(f.Training.X[j])
+			gradients := make(
