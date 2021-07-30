f9c67828013f3ce10819f9b60f10aa8977898a40
VImaginator <vimaginator@proton.me>
2021-07-30 08:16:57 +0000

    Building production.

--- pkg/graph/fitter.go
+++ pkg/graph/fitter.go
@@ -24,4 +24,4 @@ func (f Fitter) Fit(g Graph, w ...MetricsWriter) {
 			}
 			g.Minimize(gradients)
 			for k := range w {
-				w[k].Write(Metrics{Epoch: i, Sample: j, Estima
+				w[k].Write(Metrics{Epoch: i, Sample: j, Estimate: a, Actual: f.Tra
