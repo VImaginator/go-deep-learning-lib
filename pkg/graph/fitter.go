3c64829c99f127577cd374b8c3bb8d5ca821065b
VImaginator <vimaginator@proton.me>
2021-09-27 10:27:51 +0000

    new color

--- pkg/graph/fitter.go
+++ pkg/graph/fitter.go
@@ -30,4 +30,5 @@ func (f Fitter) Fit(g Graph, w ...MetricsWriter) {
 		for j := range f.Validation.X {
 			a := g.Estimate(f.Validation.X[j])
 			gradients := make([]float64, len(a))
-			for k :
+			for k := range gradients {
+				
