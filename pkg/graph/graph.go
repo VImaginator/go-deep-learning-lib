c6db39356e1dbc94f9975e9eb73e28b832927249
VImaginator <vimaginator@proton.me>
2022-05-15 08:36:27 +0000

    code restructure

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -40,4 +40,5 @@ func (g Graph) Gradients() [][][]float64 {
 
 func (g Graph) Loss(x, y []float64) []float64 {
 	a := g.Estimate(x)
-	loss := make([]float64, 
+	loss := make([]float64, len(y))
+	for i := rang
