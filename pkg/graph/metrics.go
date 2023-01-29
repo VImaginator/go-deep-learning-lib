6f43461f03264d26f96bc193992b231405b8adca
VImaginator <vimaginator@proton.me>
2023-01-29 11:52:28 +0000

    oom_kill: has_intersects_mems_allowed() needs rcu_read_lock()

--- pkg/graph/metrics.go
+++ pkg/graph/metrics.go
@@ -8,3 +8,19 @@ import (
 
 type Metrics struct {
 	Actual   []float64
+	Estimate []float64
+	Epoch    int
+	Loss     []float64
+	Sample   int
+}
+
+func (m Metrics) String() string {
+	var str = func(f []float64) string {
+		s := make([]string, len(f))
+		for k := range f {
+			s[k] = fmt.Sprintf("%.4f", f[k])
+		}
+		return fmt.Sprintf("[%s]", strings.Join(s, ", "))
+	}
+	var loss = func(a, y []float64) float64 {
+		var sum float64
