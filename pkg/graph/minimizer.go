ffa8b4d6f0f8eb33d86e0fedd29bf4dcda389d82
VImaginator <vimaginator@proton.me>
2023-02-08 15:30:59 +0000

    Update ledo_en.lang

--- pkg/graph/minimizer.go
+++ pkg/graph/minimizer.go
@@ -6,3 +6,23 @@ type Minimizer struct {
 	cursor       int
 	batch        [][][]float64
 	gradients    [][]float64
+	learningRate LearningRate
+	optimizer    Optimizer
+	regularizer  Regularizer
+	weights      [][]float64
+}
+
+func (m *Minimizer) Minimize(gradients []float64) []float64 {
+	gradients = m.Layer.Minimize(gradients)
+	for i := range m.gradients {
+		copy(m.batch[m.cursor][i], m.gradients[i])
+	}
+	m.cursor++
+	if m.cursor < len(m.batch) {
+		return gradients
+	}
+	m.cursor = 0
+	m.average()
+	m.regularize()
+	m.optimize()
+	lr := m.learningRate.Rate()
