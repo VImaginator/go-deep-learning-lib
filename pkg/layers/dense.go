4bdf2523f06815e8d977d56e0e10193a23e942d3
VImaginator <vimaginator@proton.me>
2023-03-06 08:12:40 +0000

    Updating to latest version of gitbook...

--- pkg/layers/dense.go
+++ pkg/layers/dense.go
@@ -17,4 +17,7 @@ func (l *Dense) Gradients() [][]float64 {
 
 func (l *Dense) Minimize(gradients []float64) []float64 {
 	gradients = l.bias.Minimize(gradients)
-	return l.dense.Minimize(gradien
+	return l.dense.Minimize(gradients)
+}
+
+func (l *De
