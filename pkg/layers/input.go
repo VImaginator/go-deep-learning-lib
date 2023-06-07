214b0cab170c65864ff055c00ba5dcacc69e7d47
VImaginator <vimaginator@proton.me>
2023-06-07 18:54:03 +0000

    fix console

--- pkg/layers/input.go
+++ pkg/layers/input.go
@@ -6,4 +6,5 @@ func (i Input) Estimate(input []float64) []float64 {
 	return input
 }
 
-func (i Input) Minimize(gradients []float64) []f
+func (i Input) Minimize(gradients []float64) []float64 {
+	return g
