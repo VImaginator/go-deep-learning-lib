e76caaaf241c66f90dbaacba78f9d49dd9b24b88
VImaginator <vimaginator@proton.me>
2023-06-22 06:50:11 +0000

    Add DOAP file

--- pkg/layers/layer.go
+++ pkg/layers/layer.go
@@ -8,3 +8,15 @@ type layer struct {
 }
 
 func (l *layer) Minimize(loss []float64) []float64 {
+	return loss
+}
+
+func (l *layer) Shape() []uint64 {
+	return l.outputShape.Shape()
+}
+
+func (l *layer) SetShape(shape []uint64) {
+	l.inputShape = shape
+	l.outputShape = shape
+	l.output = make([]float64, l.outputShape.Size())
+}
