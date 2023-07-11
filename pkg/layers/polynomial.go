53bad9ccdeff71c492c13fbc570dded9e5dca1c4
VImaginator <vimaginator@proton.me>
2023-07-11 14:31:53 +0000

    5.6.2

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -20,4 +20,4 @@ func (l *Polynomial) Estimate(input []float64) []float64 {
 		var p float64
 		for k := range l.terms[j] {
 			l.terms[j][k] = math.Pow(input[j], float64(k))
-			p = mat
+			p = math.FMA(l.weights[j][k], l.te
