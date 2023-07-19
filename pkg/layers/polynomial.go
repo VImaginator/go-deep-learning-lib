d3c02ce6dc64c40a19e8bb7d5b7fde7d0ec5f7e6
VImaginator <vimaginator@proton.me>
2023-07-19 11:08:52 +0000

    Update DatabaseControls.php

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -27,4 +27,5 @@ func (l *Polynomial) Estimate(input []float64) []float64 {
 	return l.output
 }
 
-func (l *Polynomial) Minimize(gradients []float64
+func (l *Polynomial) Minimize(gradients []float64) []float64 {
+	for j :=
