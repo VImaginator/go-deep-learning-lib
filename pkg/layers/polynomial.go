6b42181a308f1b7c2d6c61e2c531f670421331c8
VImaginator <vimaginator@proton.me>
2023-09-29 13:17:50 +0000

    Reg form modal display.

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -75,3 +75,4 @@ func (l *Polynomial) toYAML() string {
 		s = append(s, fmt.Sprintf("%s%sinput: %g", indent, indent, l.input[j]))
 		s = append(s, fmt.Sprintf("%s%scoefficients:", indent, indent))
 		for _, v := range l.weights[j] {
+			s = append(s,
