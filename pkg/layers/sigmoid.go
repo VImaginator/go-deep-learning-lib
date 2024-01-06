b20e1fa2138da41ed3890738e4b0a5c896780590
VImaginator <vimaginator@proton.me>
2024-01-06 06:28:56 +0000

    AUTO: Update upstream packages

--- pkg/layers/sigmoid.go
+++ pkg/layers/sigmoid.go
@@ -35,4 +35,5 @@ func (s *Sigmoid) Minimize(loss []float64) []float64 {
 func (s *Sigmoid) String() string {
 	lines := []string{"sigmoid:"}
 	lines = append(lines, fmt.Sprintf("%soutputs:", indent))
-	for _, v := range s
+	for _, v := range s.output {
+		lines = 
